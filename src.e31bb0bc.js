592039539aba02e92cac168e8ad9b1f666318a15
Natalia Lypovyk <cardinnata@gmail.com>
2021-02-22 12:02:45 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -118,7 +118,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
+'use strict';
 
+var openMenu = function openMenu() {
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('.mobile-list');
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('burger--toggle');
+    menu.classList.toggle('mobile-list--opened');
+  });
+};
+
+openMenu();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56261" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63544" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
